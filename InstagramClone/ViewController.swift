dda41d616c946d72c4957082c8174ca2efb29c8a
Adauto Oliveira <adauto.oliveira@me.com>
2020-02-03 20:09:23 -0300

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Adauto Oliveira on 03/02/20.
+//  Copyright © 2020 universotec Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
